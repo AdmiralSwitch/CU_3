df24d5a6098b0df57fc205f37ff01d4708042bb6
Switch-Thomas <admiralswitch@gmail.com>
2015-08-31 17:43:23 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scottCu3App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scottCu3App
+ */
+angular.module('scottCu3App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scottCu3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scottCu3App
+ */
+angular.module('scottCu3App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
